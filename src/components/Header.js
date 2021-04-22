38eaac87a0303477c13a210a3e1080462b17c90a
canzrcolt <canzrcolt@gmail.com>
2021-04-21 22:58:21 -0400

    just showing thor

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,13 +8,10 @@ const Header = props => (
     </div>
     <div className="content">
       <div className="inner">
-        <h1>Dimension</h1>
+        <h1>Thor and Canz</h1>
         <p>
-          A fully responsive site template designed by{' '}
-          <a href="https://html5up.net">HTML5 UP</a> and released
+          We Are Climbing
           <br />
-          for free under the{' '}
-          <a href="https://html5up.net/license">Creative Commons</a> license.
         </p>
       </div>
     </div>
